e11512b60f7eefb63934ce72d929175caadcef8d
yichikawa <yichikawa@idcf.jp>
2022-10-17 19:18:55 +0900

    main.goにハンドラおよびファンクション追加

--- main.go
+++ main.go
@@ -11,7 +11,8 @@ import(
 
 func main() {
     http.HandleFunc("/myamagata", myamagata)
-    http.ListenAndServe(":8003", nil)
+    http.HandleFunc("/yichikawa", yichikawa)
+    http.ListenAndServe(":8004", nil)
 }
 
 func myamagata(w http.ResponseWriter, r *http.Request) {
@@ -39,4 +40,28 @@ func myamagata(w http.ResponseWriter, r *http.Request) {
     t.Execute(w, body)
 }
 
+func yichikawa(w http.ResponseWriter, r *http.Request) {
+    db, err := sql.Open("mysql", "yichikawa:yichikawa_App_2022@tcp(10.35.0.59:3306)/seweb")
+                if err != nil {
+                        panic(err.Error())
+                }
+    defer db.Close()
+
+     rows, err := db.Query("SELECT body FROM yichikawa_web WHERE id=1")
+                if err != nil {
+                        panic(err.Error())
+                }
+
+    defer rows.Close()
+
+    var body string
+    rows.Next()
+    rows.Scan(&body)
+// log.Print(body)
+    t , err := template.ParseFiles("main.tpl")
+                if err != nil {
+                        panic(err.Error())
+                }
+    t.Execute(w, body)
+}
 
