c516564ec992fdaaebb2d2e4f3e85a0ccc6f28aa
vvvv <vv@aa>
2022-10-17 09:55:40 +0900

    add syatani handler

--- main.go
+++ main.go
@@ -11,7 +11,8 @@ import(
 
 func main() {
     http.HandleFunc("/myamagata", myamagata)
-    http.ListenAndServe(":8003", nil)
+    http.HandleFunc("/syatani", syatani)
+    http.ListenAndServe(":8006", nil)
 }
 
 func myamagata(w http.ResponseWriter, r *http.Request) {
@@ -38,5 +39,31 @@ func myamagata(w http.ResponseWriter, r *http.Request) {
                 }
     t.Execute(w, body)
 }
+
+func syatani (w http.ResponseWriter, r *http.Request) {
+    db, err := sql.Open("mysql", "syatani:syatani_App_2022@tcp(10.35.0.59:3306)/seweb")
+                if err != nil {
+                        panic(err.Error())
+                }
+    defer db.Close()
+
+     rows, err := db.Query("SELECT body FROM syatani_web WHERE id=1")
+                if err != nil {
+                        panic(err.Error())
+                }
+
+    defer rows.Close()
+
+    var body string
+    rows.Next()
+    rows.Scan(&body)
+// log.Print(body)
+    t , err := template.ParseFiles("syatani.tpl")
+                if err != nil {
+                        panic(err.Error())
+                }
+    t.Execute(w, body)
+}
+
 
 
